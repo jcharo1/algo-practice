db5881fa2eac0e350da78f77cf2e422c50f23b65
Justin <justinrcharo@gmail.com>
2022-06-29 10:23:15 -0500

    initialize repo

---
+++ Priority_Queues/heaps.py
@@ -0,0 +1,9 @@
+class PriorityQueue:
+
+#     insert - insert an element
+#     remove - remove an element  
+
+
+# front - returns the element at the front of the queue
+# size - returns the number of elements present in the queue
+# is_empty - returns True if there are no elements in the queue, and False otherwise
