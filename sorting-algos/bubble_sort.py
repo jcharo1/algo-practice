f94ab1612bc033f61d5cdb8ce8c0728de3595410
Justin <justinrcharo@gmail.com>
2022-06-29 17:34:11 -0500

    exercise1

---
+++ sorting-algos/bubble_sort.py
@@ -0,0 +1,16 @@
+
+# Bubble Sort Exercises  ===================
+
+Exercise 1
+# Sam records when they wake up every morning. Assuming Sam always wakes up in the same hour, use bubble sort to sort by earliest to latest.
+
+wakeup_times = [16,49,3,12,56,49,55,22,13,46,19,55,46,13,25,56,9,48,45]
+def bubble_sort_1(wakeup_times):
+    # TODO: Implement bubble sort solution
+    # for iteration in range(len(wakeup_times))
+    print(len(wakeup_times))
+
+    print(
+    range(len(wakeup_times)))
+print(bubble_sort_1(wakeup_times))
+# print ("Pass" if (wakeup_times[0] == 3) else "Fail")
