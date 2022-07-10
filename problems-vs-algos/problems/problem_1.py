68d975871f698fc3bfc258cf871101f3c9cab0ac
Justin <justinrcharo@gmail.com>
2022-07-10 00:55:21 -0500

    problem_2

--- problems-vs-algos/problems/problem_1.py
+++ problems-vs-algos/problems/problem_1.py
@@ -10,12 +10,28 @@ def sqrt(num):
        int: Floored Square Root
     """
     
-    pass
-    start = 0
+    if num == 0:
+        return 0
+    start = 1
     end = num 
-    while start< end:
+    while start <= end:
+        mid = (start+end)//2 
+        
+        if mid**2 == num:
+            return mid 
+        elif mid**2 < num:
+            start = mid + 1
+        else:
+            end = mid -1
+    return end
+    
+        
+
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
 print ("Pass" if  (4 == sqrt(16)) else "Fail")
 print ("Pass" if  (1 == sqrt(1)) else "Fail")
 print ("Pass" if  (5 == sqrt(27)) else "Fail")
+
+
+print(6**6)
